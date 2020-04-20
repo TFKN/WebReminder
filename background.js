c26105e477d46f46c235cc709d5571692f25bec4
shungokusatsu <fukand-takuya-bw@ynu.jp>
2020-04-20 09:45:11 +0900

    optionspage作成

--- background.js
+++ background.js
@@ -1,14 +1,17 @@
 /**
  * URLの更新時にチェックして
- * 指定したURLだったら音楽を再生する
+ * 指定したURLだったら音声を再生する
  */
 
 chrome.tabs.onUpdated.addListener(function(tabId, info, tab){
     console.log(tab.url);
     target_url = 'https://www.irasutoya.com/search?q=%E3%82%B9%E3%83%94%E3%83%BC%E3%82%AB%E3%83%BC';
+    url_list
     target_audio = './music/reminder.wav';
     if(info.status === "complete"){
+
         if(tab.url === target_url){
+
             var audio = new Audio();
             audio.src = target_audio;
             audio.load();
